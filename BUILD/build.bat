54454b49c50a43a4e9f07995c1f1e05b069719fa
Cycrowuk <cycrowuk@github.com>
2025-05-17 13:51:48 +0100

    Added build scripts

---
+++ BUILD/build.bat
@@ -0,0 +1,23 @@
+@Echo off
+catpck -a ../types/*.txt ../09.cat::addon2/types/
+catpck -a ../maps/oldplots/*.xml ../07.cat::addon2/maps/
+catpck -a ../maps/*.xml ../09.cat::addon2/maps/
+catpck -a ../cutscenes/8*.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/CSV*.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/Goner*.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/Khaak*.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/L2M024_Cut1.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/Terran*.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/Version*.xml ../07.cat::addon2/cutscenes/
+catpck -a ../cutscenes/LC_*.xml ../09.cat::addon2/cutscenes/
+catpck -a ../cutscenes/Beryll_*.xml ../09.cat::addon2/cutscenes/
+catpck -a ../cutscenes/Reunion_*.xml ../09.cat::addon2/cutscenes/
+catpck -a ../director/images/* ../09.cat::addon2/director/images/
+catpck -a ../director/OBS.xml ../09.cat::addon2/director/
+catpck -a ../director/SRST.xml ../09.cat::addon2/director/
+catpck -a ../t/0002* ../09.cat::addon2/t/
+catpck -a ../t/conversations.xml ../09.cat::addon2/t/
+catpck -a ../t/conversations_guilds.xml ../09.cat::addon2/t/
+catpck -a --recursive ../CAT/08/* ../08.cat
+catpck -a --recursive ../CAT/09/* ../09.cat
+spktool -createscript guilds.sps
