8e01f7d67c76a3add2975310bf26832eaff237f0
Cycrowuk <cycrowuk@github.com>
2025-06-01 11:45:28 +0100

    Added audio converter (from MP3 to WAV)

---
+++ mov/working/convert.bat
@@ -0,0 +1,25 @@
+@echo off
+SET lang=%1
+if "%lang%" == "" (
+	echo Missing language id, please specific the language to use [ie convert.bat 44]
+	exit /b
+)
+
+echo Converting voice files for language: %lang%
+echo.
+
+if not exist "ffmpeg.exe" (
+	echo Missing ffmpeg, check Readme.md for preperation info
+	exit /b
+)
+
+
+if not exist "convert\" (
+	echo Missing convert directory, create the convert directory and copy the audio files in
+	exit /b
+)
+
+FOR /F "tokens=*" %%G IN ('dir /b convert\*.mp3') DO (
+	echo Converting '%%G'...
+	ffmpeg -y -i "convert\%%G" -acodec pcm_s16le -ac 1 -ar 44100 -filter:a "volume=10dB" "%lang%\%%~nG.wav"
+)
