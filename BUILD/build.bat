453e492262ae9caa8e2ee0da9c80cd13006ddb5d
Cycrowuk <cycrowuk@github.com>
2025-10-12 14:01:54 +0100

    Updated build script to include old plot files to 07

--- BUILD/build.bat
+++ BUILD/build.bat
@@ -1,8 +1,4 @@
 @Echo off
-catpck -a ../types/*.txt ../09.cat::addon2/types/
-catpck -a ../types/*.xml ../09.cat::addon2/types/
-catpck -a ../maps/oldplots/*.xml ../07.cat::addon2/maps/
-catpck -a ../maps/*.xml ../09.cat::addon2/maps/
 catpck -a ../cutscenes/8*.xml ../07.cat::addon2/cutscenes/
 catpck -a ../cutscenes/CSV*.xml ../07.cat::addon2/cutscenes/
 catpck -a ../cutscenes/Goner*.xml ../07.cat::addon2/cutscenes/
@@ -10,6 +6,20 @@ catpck -a ../cutscenes/Khaak*.xml ../07.cat::addon2/cutscenes/
 catpck -a ../cutscenes/L2M024_Cut1.xml ../07.cat::addon2/cutscenes/
 catpck -a ../cutscenes/Terran*.xml ../07.cat::addon2/cutscenes/
 catpck -a ../cutscenes/Version*.xml ../07.cat::addon2/cutscenes/
+catpck -a "../director/1.40 *.xml" ../07.cat::addon2/director/
+catpck -a "../director/3.02 *.xml" ../07.cat::addon2/director/
+catpck -a "../director/3.05 *.xml" ../07.cat::addon2/director/
+catpck -a "../director/3.06 *.xml" ../07.cat::addon2/director/
+catpck -a "../director/3.09 *.xml" ../07.cat::addon2/director/
+catpck -a ../director/AP_DoubleAgent.xml ../07.cat::addon2/director/
+catpck -a ../director/CSV_*.xml ../07.cat::addon2/director/
+catpck -a ../director/2.0*.xml ../07.cat::addon2/director/
+catpck -a ../director/Reunion*.xml ../07.cat::addon2/director/
+catpck -a ../director/start_oldplot.xml ../07.cat::addon2/director/
+catpck -a ../maps/oldplots/*.xml ../07.cat::addon2/maps/
+catpck -a ../types/*.txt ../09.cat::addon2/types/
+catpck -a ../types/*.xml ../09.cat::addon2/types/
+catpck -a ../maps/*.xml ../09.cat::addon2/maps/
 catpck -a ../cutscenes/LC_*.xml ../09.cat::addon2/cutscenes/
 catpck -a ../cutscenes/Beryll_*.xml ../09.cat::addon2/cutscenes/
 catpck -a ../cutscenes/Reunion_*.xml ../09.cat::addon2/cutscenes/
